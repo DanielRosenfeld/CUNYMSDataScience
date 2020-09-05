17ed5f8aabb5677c1bad6b0fa2bd9911d749a7ff
Daniel Rosenfeld <drosenfeld371@gmail.com>
2020-09-05 03:43:43 -0400

    Add files via upload

---
+++ movie_pref.sql
@@ -0,0 +1,10 @@
+SELECT *
+FROM `Movie Poll! Data 607 Assignment 2 (Responses) - Form Responses 1`;
+
+ALTER TABLE `Movie Poll! Data 607 Assignment 2 (Responses) - Form Responses 1`
+	ADD person_id INT AUTO_INCREMENT PRIMARY KEY;
+    
+ALTER TABLE `Movie Poll! Data 607 Assignment 2 (Responses) - Form Responses 1`
+	ADD PRIMARY KEY (person_id);
+
+ 
